a681213f38fcef1682566f67138b2539de85c42d
Dangertrip <dangertrip@hotmail.com>
2017-07-15 17:58:27 -0500

    0.0.1

--- epi/epivis/util/GetNode.py
+++ epi/epivis/util/GetNode.py
@@ -1,9 +1,22 @@
 import subprocess
 
+'''
+Return all the nodes' name as a string slice from qhost command.
+Ignore all nodes info if 'ARCH' attribute of nodes is '-'
+'''
 def GetNode():
-    out = subprocess.call('qhost',shell=True)
-    lines = out.readlines()
-    print(lines)
+    out = subprocess.Popen('qhost',shell=True,stdout=subprocess.PIPE)
+    s=out.stdout.readlines()[3:]
+#    lines = out.readlines()
+#    print(out)
+    #lines = s.decode('utf-8').split('\n')[3:]
+    nodes=[]
+    for line in s:
+        t = line.decode('utf-8').strip().split()
+        #print(t)
+        if t[1]!='-':
+            nodes.append(t[0])
+    return(nodes)
 
 if __name__=="__main__":
-    GetNode()
+    print(GetNode())
